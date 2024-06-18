bd891e3d775cc999a485b65243cc1940edeb798b
Joshua Figueroa <joshuavillartafigueroa@gmail.com>
2024-06-19 02:29:35 +0800

    Initial commit

---
+++ HomeLink/HomeLink/HomeLinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeLinkApp.swift
+//  HomeLink
+//
+//  Created by Joshua Figueroa on 6/18/24.
+//
+
+import SwiftUI
+
+@main
+struct HomeLinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
